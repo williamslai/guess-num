e08159aaac5ff9b062f72e39389b9c7abcbb204f
William <williams0427@gmail.com>
2018-08-02 23:23:35 +0800

    count v1.0  teacher

--- guess-num.py
+++ guess-num.py
@@ -5,13 +5,17 @@
 #猜錯，要告訴他 把答案大/小
 import random
 r = random.randint(1, 100)
+count = 0
 while True:
 	num = input('請輸入數字： ')
 	num = int(num)
+	count += 1 #count = count + 1
 	if num == r:
 		print('你猜對了')
+		print('你猜了',count, '次')
 		break
 	elif num > r:
 		print('比答案大')
 	elif num < r:
 		print('比答案小')
+	print('你猜了',count, '次')
